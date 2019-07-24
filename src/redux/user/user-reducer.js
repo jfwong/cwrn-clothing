49018f494b4f5d31c5c0a468d6978779d35caa5f
jfwong798 <jfwong798@gmail.com>
2019-07-24 14:56:35 +0800

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/redux/user/user-reducer.js
+++ src/redux/user/user-reducer.js
@@ -8,11 +8,9 @@ const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case userActionTypes.SET_CURRENT_USER:
       return { ...state, currentUser: action.payload }
-      break
 
     default:
       return state
-      break
   }
 }
 
