dcff2103602b7737786846e0b2a96c5b67292772
jfwong798 <jfwong798@gmail.com>
2019-07-26 14:27:40 +0800

    moved our directory and shop data into its own reducer, craeted respective selectors and updated corresponding components with redux flow for directory/shop

--- src/components/cart-icon/index.js
+++ src/components/cart-icon/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 
@@ -17,8 +18,8 @@ const CartIcon = ({ toggleCartHidden, itemCount }) => {
   )
 }
 
-const mapStateToProps = state => ({
-  itemCount: selectCartItemCount(state),
+const mapStateToProps = createStructuredSelector({
+  itemCount: selectCartItemCount,
 })
 
 const mapDispatchToProps = dispatch => ({
